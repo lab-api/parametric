107ee88a4b0c8640a694379b652c475adbde19ae
Robbie Fasano <robert.j.fasano@colorado.edu>
2019-10-11 14:13:21 -0600

    Add methods to host or connect to one side of a ZMQ pair

--- parametric/instrument.py
+++ parametric/instrument.py
@@ -9,3 +9,17 @@ class Instrument:
         param = self.base_parameter(name, value=value, get_cmd=get_cmd, set_cmd=set_cmd, get_parser=get_parser)
         setattr(self, name, param)
         self.parameters[name] = param
+
+    def connect(self, address):
+        pass
+        
+    @classmethod
+    def remote(cls, address):
+        ''' Open and return a mimicked Instrument on a remote PC. '''
+        from parametric import Remote
+        return Remote(cls, address)
+
+    def host(self, address):
+        ''' Make the device accessible over ZMQ on the specified address. '''
+        from parametric import Local
+        self.local = Local(self, address)
