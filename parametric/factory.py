e98835ddc6e516c1d1ac26812bf91a584c668c5c
Robert Fasano <robert.j.fasano@colorado.edu>
2020-02-14 14:07:24 -0700

    Add Selector subclass

--- parametric/factory.py
+++ parametric/factory.py
@@ -11,3 +11,20 @@ class Switch(Parameter):
 class Measurement(Parameter):
     def __init__(self, name, get_cmd, *args, **kwargs):
         super().__init__(name=name, get_cmd=get_cmd, *args, **kwargs)
+
+class Selector(Parameter):
+    def __init__(self, name, value, options, *args, **kwargs):
+        self.options = options
+        self.type = type(value)
+        super().__init__(name=name, value=value, *args, **kwargs)
+
+    def set(self, value):
+        value = self.type(value)
+        if not value in self.options:
+            raise ValueError(f'Value should be one of {self.options}.')
+
+        self.value = value
+        if self.set_cmd is not None:
+            self.set_cmd(value)
+        for callback in self.callbacks.values():
+            callback(value)
